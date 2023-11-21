e0b5c10ec506f5286823d9810abfdc196324369e
Gustavo Ocanto <gustavoocanto@gmail.com>
2023-11-21 15:24:12 +0800

    start working on router

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"time"
+)
+
+func main() {
+	fmt.Println("gus")
+
+	app := fiber.New(fiber.Config{
+		AppName:     "Webhook Redirector",
+		IdleTimeout: time.Second * 30,
+		ReadTimeout: time.Second * 30,
+		Prefork:     true,
+	})
+
+	app.Get("/all", func(c *fiber.Ctx) error {
+		return c.SendString("here!")
+	})
+
+	fmt.Println("listening ...")
+
+	if result := app.Listen("localhost:8081"); result != nil {
+		fmt.Println("error ...")
+	}
+}
