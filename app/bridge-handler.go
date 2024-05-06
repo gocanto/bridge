45af170e857c4749dd55e7fed16d8b2f8e78f8e2
Gustavo Ocanto <gustavoocanto@gmail.com>
2024-05-06 16:19:21 +0800

    wip

--- app/bridge-handler.go
+++ app/bridge-handler.go
@@ -1,34 +1,70 @@
 package bridge
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
-	"strings"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	// Extract service identifier from the URL path
-	path := strings.Trim(r.URL.Path, "/")
-	var redirectURL string
+type HandlerResponse struct {
+	Message string `json:"message"`
+}
 
-	switch path {
-	case "service1":
-		redirectURL = "http://example.com/service1"
-	case "service2":
-		redirectURL = "http://example.com/service2"
-	default:
-		// If the URL does not specify, use the header
-		sender := r.Header.Get("X-Sender-ID")
-		switch sender {
-		case "sender1":
-			redirectURL = "http://example.com/service1"
-		case "sender2":
-			redirectURL = "http://example.com/service2"
-		default:
-			http.Error(w, "Unauthorized sender or unknown service", http.StatusUnauthorized)
-			return
-		}
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL, r.Body, r.RequestURI, &r.Response, r.Context(), &r)
+
+	//payload := Payload{
+	//	request: r,
+	//	writer:  &w,
+	//	body:    r.Body,
+	//	content: content,
+	//}
+	//
+	//content, err := r.Body.Read(payload.content)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//// Extract service identifier from the URL path
+	//path := strings.Trim(r.URL.Path, "/")
+	//var redirectURL string
+	//
+	////r.Header
+	//
+	//switch path {
+	//case "service1":
+	//	redirectURL = "http://example.com/service1"
+	//case "service2":
+	//	redirectURL = "http://example.com/service2"
+	//default:
+	//	// If the URL does not specify, use the header
+	//	sender := r.Header.Get("X-Sender-ID")
+	//	switch sender {
+	//	case "sender1":
+	//		redirectURL = "http://example.com/service1"
+	//	case "sender2":
+	//		redirectURL = "http://example.com/service2"
+	//	default:
+	//		http.Error(w, "Unauthorized sender or unknown service", http.StatusUnauthorized)
+	//		return err
+	//	}
+	//}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	handlerResponse := HandlerResponse{Message: "Ok"}
+	response, err := json.Marshal(handlerResponse)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(fmt.Sprintf("%s", err)))
+		return
 	}
 
+	_, _ = w.Write(response)
+
+	//return json.NewEncoder(w).Encode(payload)
+
 	// Redirect to the determined URL
-	http.Redirect(w, r, redirectURL, http.StatusFound)
+	//http.Redirect(w, r, redirectURL, http.StatusFound)
 }
